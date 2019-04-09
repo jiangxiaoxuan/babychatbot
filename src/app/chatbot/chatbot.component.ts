fc7033f9a78ce036ca32a034d234c8bcf6b71362
Xiaoxuan <freyajiang0814@gmail.com>
2019-04-08 18:07:46 -0700

    Created First UI draft

---
+++ src/app/chatbot/chatbot.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-chatbot',
+  templateUrl: './chatbot.component.html',
+  styleUrls: ['./chatbot.component.css']
+})
+export class ChatbotComponent implements OnInit {
+
+  name = "Andy";
+
+  chatMessages: string[] = [
+    "Hello",
+    "How are you?"
+  ]
+
+  userInput: string;
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  onEnter() {
+    console.log(this.userInput);
+    console.log(this.chatMessages);
+    this.chatMessages.push(this.userInput);
+    this.userInput = "";
+  }
+}
