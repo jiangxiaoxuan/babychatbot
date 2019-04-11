aa736251af2ee58269437b5e039316b1a5e3aaae
Xiaoxuan <freyajiang0814@gmail.com>
2019-04-11 16:29:31 -0700

    Modified math questions

--- src/app/mock-quizzes.ts
+++ src/app/mock-quizzes.ts
@@ -1,14 +1,14 @@
 import { Mathquiz } from './mathquiz';
 
 export const QUIZZES: Mathquiz[] = [
-  { question: '1', answer: '1' },
-  { question: '2', answer: '2' },
-  { question: '3', answer: '3' },
+  { question: '1 + 1', answer: '2' },
+  { question: '1 + 999', answer: '1000' },
+  { question: '25 * 25', answer: '625' },
   { question: '4 + 1 = ?', answer: '5' },
-  { question: '5', answer: '5' },
-  { question: '6', answer: '6' },
-  { question: '7', answer: '7' },
-  { question: '8', answer: '8' },
-  { question: '9', answer: '9' },
-  { question: '10', answer: '10' }
+  { question: '5 * 9', answer: '45' },
+  { question: '0 * 76', answer: '0' },
+  { question: '1991 * 1', answer: '1991' },
+  { question: '8 + 2', answer: '10' },
+  { question: '2006 + 10', answer: '2016' },
+  { question: '10 + 0', answer: '10' }
 ];
